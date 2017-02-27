35076fb9cea0370b2e781f4b2cd74aa9d7cd1b55
Leandro Bolivar <leandrob131@gmail.com>
2017-02-26 21:39:59 -0500

    Added validateBooks function to validated example

--- src/main/scala/fp/validated/BookValidationService.scala
+++ src/main/scala/fp/validated/BookValidationService.scala
@@ -1,10 +1,11 @@
 package fp.validated
 
-import cats.data.ValidatedNel
+import cats.data.{NonEmptyList, ValidatedNel}
 import cats.syntax.cartesian._
+import cats.syntax.semigroup._
 import cats.syntax.validated._
 import fp.Genre.InvalidGenre
-import fp.{Book, Genre, InvalidParameter}
+import fp.{Book, EmptyBookList, Genre, InvalidParameter}
 
 import scala.util.matching.Regex
 
@@ -13,13 +14,17 @@ trait BookValidationService {
   private val isbnRegex: Regex =
     """ISBN(?:-13)?:?\x20*(?=.{17}$)97(?:8|9)([ -])\d{1,5}\1\d{1,7}\1\d{1,6}\1\d$""".r
 
+  def validateBooks(bs: List[Book]) = bs match {
+    case Nil => EmptyBookList("Book list was empty").invalidNel[Book]
+    case books => books map validateBook reduce (_ |+| _)
+  }
 
-  def validateBook(b: Book) = ( validateIsbn(b.isbn) |@|
+  def validateBook(b: Book): ValidatedNel[InvalidParameter, NonEmptyList[Book]] = ( validateIsbn(b.isbn) |@|
     validateAuthor(b.author) |@|
     validateTitle(b.title) |@|
     validateGenre(b.genre) ) map {
     case (isbn, author, title, genre) =>
-      Book(isbn, title, author, genre)
+      NonEmptyList.of(Book(isbn, title, author, genre))
   }
 
   private def validateGenre(g: Genre): ValidatedNel[InvalidParameter, Genre] = g match {
